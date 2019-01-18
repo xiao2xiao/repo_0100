6018805e54e3f3df30eef8d2ff4754e5192e189f
xiao2xiao <976822314@qq.com>
2019-01-18 21:09:23 +0800

    add spring-boot-cloud-demo添加了spring boot和cloud所需要的jar包

---
+++ spring-boot-cloud-demo/src/main/java/com/neno/AppApplication.java
@@ -0,0 +1,15 @@
+package com.neno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author root
+ * @date
+ */
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
