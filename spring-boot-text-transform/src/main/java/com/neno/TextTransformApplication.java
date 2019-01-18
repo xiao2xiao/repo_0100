de9d6751aeab946bba752c903f97d88224fef5fe
xiao2xiao <976822314@qq.com>
2019-01-18 17:04:55 +0800

    add spring-boot-text-transform文本转化

---
+++ spring-boot-text-transform/src/main/java/com/neno/TextTransformApplication.java
@@ -0,0 +1,15 @@
+package com.neno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author root
+ * @date
+ */
+@SpringBootApplication
+public class TextTransformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TextTransformApplication.class, args);
+    }
+}
